4c4f03cfb8c351a36bfc237ac5374408e70dd5a6
Horppyman <horppyboi1@gmail.com>
2021-04-24 12:25:43 +0100

    Add products api and display on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,40 @@
-import Head from 'next/head';
+import { useState } from "react";
+import Head from "next/head";
 
+import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
+import { getData } from "../utils/fetchData";
+import ProductItem from "../components/product/ProductItem";
 
-export default function Home() {
+export default function Home(props) {
+  const [products, setProducts] = useState(props.products);
+  // console.log(products);
+
   return (
     <>
       <Head>
         <title>E-commerce | Home</title>
       </Head>
       <Navbar />
-      Home
+      <Layout>
+        <div className="product">
+          {products.length === 0 && <h2>No product</h2>}
+          {products.length &&
+            products.map((product) => (
+              <ProductItem key={product._id} product={product} />
+            ))}
+        </div>
+      </Layout>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await getData("product");
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    }, // will be passed to the page component as props
+  };
 }

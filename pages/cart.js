c8176561ff19f16c3c07f83ab4b062da45fcc38b
Horppyman <horppyboi1@gmail.com>
2021-05-03 22:07:19 +0100

    improve cart functionalities

--- pages/cart.js
+++ pages/cart.js
@@ -1,17 +1,36 @@
-import Head from 'next/head';
+import Head from "next/head";
+import { useContext } from "react";
 
 import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
+import { DataContext } from "../store/GlobalState";
 
 export default function Cart() {
+  const { state, dispatch } = useContext(DataContext);
+  const { cart } = state;
+
+  if (cart.length === 0)
+    return (
+      <>
+        <Head>
+          <title>E-commerce | Cart</title>
+        </Head>
+        <Navbar />
+        <Layout>
+          <img className='img-responsive w-50 h-50' src="/empty-cart.svg" alt="empty" />
+        </Layout>
+      </>
+    );
+
   return (
     <>
-     <Head>
-      <title>E-commerce | Cart</title>
-    </Head>
-    <Navbar />
-    <div className="container">
-    Cart
-    </div>
+      <Head>
+        <title>E-commerce | Cart</title>
+      </Head>
+      <Navbar />
+      <Layout>
+        <h1>Cart</h1>
+      </Layout>
     </>
-  )
+  );
 }
